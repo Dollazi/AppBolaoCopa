96d856c6cd2c053a1d76d9a877502f7c50866283
Luis Roberto Moreno Dollazi <luis.moreno.dol@gmail.com>
2023-01-19 23:34:46 -0300

    Criação da aplicação web com a finalização do banco de dados para acessar todas informações úteis via api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
     log: ['query'],
@@ -9,7 +11,7 @@ const prisma = new PrismaClient({
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
-    })  
+    })
 
     await fastify.register(cors, {
         origin: true,
@@ -18,19 +20,57 @@ async function bootstrap() {
     // http://127.0.0.1:3333/pools/count
 
     fastify.get('/pools/count', async () => {
-/*         const pools = await prisma.pool.findMany({
-            where: {
-                code: {
-                    startsWith: ''
-                }
-            }
-        }) */
+        /*         const pools = await prisma.pool.findMany({
+                    where: {
+                        code: {
+                            startsWith: ''
+                        }
+                    }
+                }) */
 
         const count = await prisma.pool.count()
 
         return { count }
     })
-    
+
+    fastify.get('/users/count', async () => {
+
+        const count = await prisma.user.count()
+
+        return { count }
+    })
+
+    fastify.get('/guesses/count', async () => {
+
+        const count = await prisma.guess.count()
+
+        return { count }
+    })
+
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        })
+
+
+        const { title } = createPoolBody.parse(request.body)
+
+        const generate = new ShortUniqueId({ length: 6 })
+
+        const code = String(generate()).toUpperCase()
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code: code,
+            }
+        })
+
+        return reply.status(201).send({ code })
+
+        //return { title }
+    })
+
     await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
